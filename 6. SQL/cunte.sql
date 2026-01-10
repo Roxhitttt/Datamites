0f8892f2783da1e7dd1209e9828005ad418e5bce
Rohit Bava <rohit02032001@gmail.com>
2026-01-10 16:29:18 +0530

    co

---
+++ 6. SQL/cunte.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE cunt;
+
+USE cunt;
+
+DROP DATABASE cunt;
+
+CREATE TABLE cunt_details(
+cunt_id INT,
+cunt_name VARCHAR(10),
+cunt_progession VARCHAR(50),
+cunt_salary FLOAT
+);
+
+INSERT INTO cunt_details VALUES
+(101, "Ram", "Software", 35000.00),
+(102, "Sham", "Data Science", 39000.00),
+(103, "Aditya", "AI", 40000.00),
+(104, "Prajwal", "IOT", 45000.00),
+(105, "Vishal", "any", 50000.00),
+(106, "Ishwar", "Computer", 60000.00),
+(107, "Arjun", "HR", 70000.00);
+
+
+TRUNCATE TABLE cunt_datails;
+
+SELECT * FROM cunt_details;
+
+
+
